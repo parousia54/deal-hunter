d367d61fba07891614822d8be393834e83623d12
stephen.rmit <stephen.rmit@gmail.com>
2012-11-24 12:24:53 +0000

    Update

--- DealHunterClient/Sensis Hackathorn/2012/src/com/pinkdroid/dealshunter/screen/view/DealListViewAdapter.java
+++ DealHunterClient/Sensis Hackathorn/2012/src/com/pinkdroid/dealshunter/screen/view/DealListViewAdapter.java
@@ -3,13 +3,18 @@ package com.pinkdroid.dealshunter.screen.view;
 import java.util.List;
 
 import com.pinkdroid.dealshunter.R;
+import com.pinkdroid.dealshunter.model.Business;
 import com.pinkdroid.dealshunter.model.Deal;
+import com.pinkdroid.dealshunter.model.Deal.UserFeedback;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DealListViewAdapter extends BaseAdapter{
 	private Context context;
@@ -43,8 +48,46 @@ public class DealListViewAdapter extends BaseAdapter{
 			LayoutInflater inflater = (LayoutInflater) context
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			convertView = inflater.inflate(R.layout.deal_listview_row_layout, null);
+			//bindDataToView(convertView, dealList.get(position));
 		}
 		return convertView;
 	}
-
+	
+	private void bindDataToView(View convertView, Deal deal){
+		if(deal!=null){
+			//Todo:Download images to put into the Image View here (or can use it online)
+			TextView businessNameTV, distanceTV, addressTV, discountDescriptionTV, thumbUpTV, thumbDownTV, endTimeTV;
+			ImageView businessLogo, verifiedIcon, dealImage;
+			
+			businessNameTV = (TextView) convertView.findViewById(R.id.deal_listview_row_business_name);
+			distanceTV = (TextView) convertView.findViewById(R.id.deal_listview_row_distance);
+			addressTV = (TextView) convertView.findViewById(R.id.deal_listview_row_business_address);
+			discountDescriptionTV = (TextView) convertView.findViewById(R.id.deal_listview_row_description);
+			thumbUpTV = (TextView) convertView.findViewById(R.id.deal_listview_row_thumbup);
+			thumbDownTV = (TextView) convertView.findViewById(R.id.deal_listview_row_thumbdown);
+			endTimeTV = (TextView) convertView.findViewById(R.id.deal_listview_row_endtime);
+			
+			businessLogo = (ImageView) convertView.findViewById(R.id.deal_listview_row_business_icon);
+			verifiedIcon = (ImageView) convertView.findViewById(R.id.deal_listview_row_verified_icon);
+			dealImage = (ImageView) convertView.findViewById(R.id.deal_listview_row_icon);
+			
+			Business business = deal.getBusinessInfo();
+			businessNameTV.setText(business.getBusinessName());
+			addressTV.setText(business.getAddress().toString());
+			businessLogo.setImageURI(Uri.parse(business.getLogoUrl()));
+			//Todo; download and set logo here
+			
+			discountDescriptionTV.setText(deal.getDiscountDescription());
+			distanceTV.setText(String.valueOf(deal.getDistance())+"km away");
+			
+			UserFeedback feedback = deal.getFeedback();
+			thumbUpTV.setText(feedback.getThumbUpNo());
+			thumbDownTV.setText(feedback.getThumbDownNo());
+			
+			endTimeTV.setText(deal.getEndTime().toString());
+			dealImage.setImageURI(Uri.parse(deal.getImageUrl()));
+			verifiedIcon.setImageResource(R.drawable.verified);
+		}
+		
+	}
 }
