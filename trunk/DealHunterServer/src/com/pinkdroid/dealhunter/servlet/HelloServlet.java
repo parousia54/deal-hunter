002ac48bebe038a57e1b6edc88910ee466360029
myselfrajesh@gmail.com <myselfrajesh@gmail.com@a1faf5c3-285b-e2ec-1406-0b6de0425506>
2012-11-24 07:34:32 +0000

    First commit

---
+++ trunk/DealHunterServer/src/com/pinkdroid/dealhunter/servlet/HelloServlet.java
@@ -0,0 +1,38 @@
+package com.pinkdroid.dealhunter.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Hello fucker!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ trunk/DealHunterServer/src/com/pinkdroid/dealhunter/util/GSONUtil.java
@@ -0,0 +1,15 @@
+package com.pinkdroid.dealhunter.util;
+
+import com.google.gson.Gson;
+import com.pinkdroid.dealhunter.model.User;
+
+public class GSONUtil {
+	
+	static Gson gson;
+	public static String UsertoJSON(User obj)
+	{
+		String json = gson.toJson(obj);
+		return json;
+	}
+
+}
