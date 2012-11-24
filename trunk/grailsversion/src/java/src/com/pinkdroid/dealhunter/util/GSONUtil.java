c4714e3e494fd2ba900a16a31e4e198c15a42aa4
myselfrajesh@gmail.com <myselfrajesh@gmail.com@a1faf5c3-285b-e2ec-1406-0b6de0425506>
2012-11-24 16:23:03 +0000

    getdealsforbusiness

--- trunk/grailsversion/src/java/src/com/pinkdroid/dealhunter/util/GSONUtil.java
+++ trunk/grailsversion/src/java/src/com/pinkdroid/dealhunter/util/GSONUtil.java
@@ -1,6 +1,11 @@
 package src.com.pinkdroid.dealhunter.util;
 
+import java.io.File;
+import java.util.Date;
+
 import com.google.gson.Gson;
+import com.mongodb.DBObject;
+
 import src.com.pinkdroid.dealhunter.model.Business;
 import src.com.pinkdroid.dealhunter.model.Deal;
 
@@ -16,6 +21,27 @@ public class GSONUtil {
 	public static String dealToJSON(Deal deal) {
 		String json = gson.toJson(deal);
 		return json;
+	}
+
+	public static Deal jsonToDeal(DBObject next) {
+		// String id;
+		// String dealTitle;
+		// String dealDescription;
+		// File dealImage;
+		// Date dealStartDate;
+		// Date dealEndDate;
+		// int dealScore; // similar to reddit upvote/downvote score
+		// String businessId;
+
+		Deal deal = new Deal();
+		deal.setDealTitle((String)next.get("dealTitle"));
+		deal.setDealDescription((String)next.get("dealDescription"));
+		deal.setDealStartDate(((Date)next.get("dealStartDate")));
+		deal.setDealEndDate(((Date)next.get("dealEndDate")));
+		deal.setDealScore((int) next.get("dealScore"));
+		deal.setDealImage((File) next.get("dealImage"));
+		
+		return deal;
 	}
 
 }
