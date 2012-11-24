1713d4cf15d2195cc7c360627d369ff429abd64b
myselfrajesh <myselfrajesh@gmail.com>
2012-11-24 16:38:36 +0000

    DBObject to business

--- grailsversion/src/java/src/com/pinkdroid/dealhunter/util/GSONUtil.java
+++ grailsversion/src/java/src/com/pinkdroid/dealhunter/util/GSONUtil.java
@@ -3,11 +3,12 @@ package src.com.pinkdroid.dealhunter.util;
 import java.io.File;
 import java.util.Date;
 
-import com.google.gson.Gson;
-import com.mongodb.DBObject;
-
+import src.com.pinkdroid.dealhunter.model.Address;
 import src.com.pinkdroid.dealhunter.model.Business;
 import src.com.pinkdroid.dealhunter.model.Deal;
+
+import com.google.gson.Gson;
+import com.mongodb.DBObject;
 
 public class GSONUtil {
 
@@ -23,30 +24,38 @@ public class GSONUtil {
 		return json;
 	}
 
-	public static Deal jsonToDeal(DBObject next) {
-		// String id;
-		// String dealTitle;
-		// String dealDescription;
-		// File dealImage;
-		// Date dealStartDate;
-		// Date dealEndDate;
-		// int dealScore; // similar to reddit upvote/downvote score
-		// String businessId;
-
+	public static Deal dbObjectToDeal(DBObject next) {
 		Deal deal = new Deal();
-		deal.setDealTitle((String)next.get("dealTitle"));
-		deal.setDealDescription((String)next.get("dealDescription"));
-		deal.setDealStartDate(((Date)next.get("dealStartDate")));
-		deal.setDealEndDate(((Date)next.get("dealEndDate")));
-		deal.setDealScore((Integer) next.get("dealScore"));
+		deal.setDealTitle((String) next.get("dealTitle"));
+		deal.setDealDescription((String) next.get("dealDescription"));
+		deal.setDealStartDate(((Date) next.get("dealStartDate")));
+		deal.setDealEndDate(((Date) next.get("dealEndDate")));
+		deal.setDealScore((int) next.get("dealScore"));
 		deal.setDealImage((File) next.get("dealImage"));
-		
+
 		return deal;
 	}
 
-    public static Business jsonToBusiness(DBObject next) {
-        Business business = new Business();
-        business.setUsername((String) next.get("username"));
-        return business;
-    }
+	public static Business dbObjectToBusiness(DBObject next) {
+		Business business = new Business();
+		business.setUsername((String) next.get("username"));
+		business.setVerified((boolean) next.get("isVerfied"));
+		business.setBusinessCategory((String) next.get("businessCategory"));
+		business.setBusinessImage((File)next.get("bussinessImage"));
+		business.setBusinessPhone((String)next.get("businessPhone"));
+		
+		Address bAddress = new Address();
+		DBObject address = (DBObject) next.get("businessAddress");
+		bAddress.setPostCode((String) address.get("postCode"));
+		bAddress.setState((String) address.get("state"));
+		bAddress.setStreetName((String) address.get("streetName"));
+		bAddress.setStreetNumber((String) address.get("streetNumber"));
+		bAddress.setSuburb((String) address.get("suburb"));
+		
+		business.setBusinessAddress(bAddress);
+		
+		return business;
+
+	}
+
 }
